a1e84390d047ff3ca6a8ef1e1fc030dfecab7fbb
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2024-11-05 21:46:24 +0600

    2 solution codeforces

--- week2/day2/1703_C_Cypher_codeforces.cpp
+++ week2/day2/1703_C_Cypher_codeforces.cpp
@@ -1,3 +1,11 @@
+//============================================================================
+// Problem     : https://codeforces.com/problemset/problem/1703/C
+// Name        : 1703_C_Cypher_codeforces.cpp
+// Author      : Salman Sadik Siddiquee
+// Version     : 1
+// Difficuly   : 800
+//============================================================================
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- week2/day2/1722_B_Colourblindness_codeforces.cpp
+++ week2/day2/1722_B_Colourblindness_codeforces.cpp
@@ -1,3 +1,11 @@
+//============================================================================
+// Problem     : https://codeforces.com/problemset/problem/1722/B
+// Name        : 1722_B_Colourblindness_codeforces.cpp
+// Author      : Salman Sadik Siddiquee
+// Version     : 1
+// Difficuly   : 800
+//============================================================================
+
 #include <bits/stdc++.h>
 using namespace std;
 
